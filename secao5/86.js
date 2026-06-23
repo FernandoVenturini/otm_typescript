e29e828a11fc0eaf2efc6769a9a2f3565c95938d
Fernando <fernandoventurini9@hotmail.com>
2026-06-23 21:35:52 +0100

    reduce

--- secao5/86.js
+++ secao5/86.js
@@ -2,7 +2,15 @@
 
 // O método reduce() executa uma função para cada elemento do array, resultando em um único valor.
 
-const numeros = [1, 2, 3, 4, 5]; // Array de números
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // Array de números
+
+const pessoas =[
+    { nome: "Alice", idade: 30 },
+    { nome: "Bob", idade: 25 },
+    { nome: "Charlie", idade: 35 },
+    { nome: "David", idade: 20 }
+];
 
 // SOMAE TODOS OS NUMEROS DO ARRAY(REDUCE):
 const soma = numeros.reduce((acumulador, valor, indice, array) => { // A função de callback recebe quatro argumentos: acumulador, valor atual, índice e o array original
@@ -23,3 +31,25 @@ const total = numeros.reduce(function(acumulador, valor, indice, array) { // A f
         return acumulador; // Se for par, retorna o acumulador sem alterações    
 }, []);
 console.log(total); // Saída: [2, 4]
+
+const totalImpar = numeros.reduce(function(acumulador, valor, indice, array) { // A função de callback recebe quatro argumentos: acumulador, valor atual, índice e o array original
+    if (valor % 2 !== 0) acumulador.push(valor) // Se for ímpar, adiciona o valor ao acumulador
+        return acumulador; // Se for ímpar, retorna o acumulador sem alterações
+}, []);
+console.log(totalImpar); // Saída: [1, 3, 5, 7, 9]
+
+// RETORNE A PESSOA MAIS VELHA:
+const maisVelha = pessoas.reduce((acumulador, pessoa) => {
+    if (acumulador.idade > pessoa.idade) return acumulador; // Compara a idade da pessoa atual com a idade da pessoa mais velha encontrada até agora
+    return pessoa; // Retorna a pessoa mais velha comparando as idades
+});
+console.log(maisVelha); // Saída: { nome: "Charlie", idade: 35 }
+
+
+const numeros2 = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+const numerosPares = numeros2
+    .filter(valor => valor % 2 === 0)
+    .map(valor => valor * 2)
+    .reduce((ac, valor) => ac + valor);
+
+console.log(numerosPares);
