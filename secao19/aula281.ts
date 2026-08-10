d9555ce2c00995ad6192b9688214dae721a4600a
Fernando <fernandoventurini9@hotmail.com>
2026-08-10 21:15:03 +0100

    tipos literais

---
+++ secao19/aula281.ts
@@ -0,0 +1,14 @@
+// Tipos Literais
+
+const person = {
+    name: 'John',
+    age: 30,
+    isStudent: false
+}
+person.name = 'Jane'; // válido
+// person.name = 123; // inválido, pois name é do tipo string
+
+function printPerson(person: { name: string; age: number; isStudent: boolean }) {
+    return `Name: ${person.name}, Age: ${person.age}, Is Student: ${person.isStudent}`;
+}
+console.log(printPerson(person)); // Name: Jane, Age: 30, Is Student: false
